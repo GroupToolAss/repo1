2edd557b59d8b8d44e9d2c368cc07beff0a6ae1d
bishopone <101429140+bishopone@users.noreply.github.com>
2022-05-11 13:27:20 -0700

    eyosiyas aligas
    the second commit version 1.1

--- group_members.cpp
+++ group_members.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main(){
     
-    cout<<"abeselom solomon................1200738";
-
+    cout<<"Abeselom Solomon................1200738";
+    cout<<"Eyosiyas Aligas.................1201635";
 
     return 0;
 }
